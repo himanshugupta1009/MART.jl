eed875f4c37eb67b986c512d55db8e4a9aaf2d9f
himanshugupta1009 <himanshu.gupta@colorado.edu>
2023-10-03 16:26:33 -0600

    Add demo code for Interpolation; Add packages to Project.toml

--- src/utils.jl
+++ src/utils.jl
@@ -1,4 +1,6 @@
 using Dates
+import GridInterpolations as GI
+import HDF5
 
 function interpolate_value(ds1, ds2, field, t)
     df = dateformat"y-m-d_H:M:SZ"
@@ -9,4 +11,21 @@ function interpolate_value(ds1, ds2, field, t)
     time_diff = (t_ds2 - t_ds1)*(0.001)/Millisecond(1)  #Gives time differewnce between the two dates in seconds
     interpolated_values = ( (ds2[field] - ds1[field])*(t/time_diff) ) + ds1[field]
     return interpolated_values
+end
+
+function interpolate_using_GI
+
+    #Demo code for interpolation. Poorly written. Modify it when dataset becomes clearer.
+    x_values = [i for i in 1:300]
+    y_values = [i for i in 1:300]
+    z_values = [i for i in 1:50]
+    grid = GI.RectangleGrid(x_values,y_values,z_values)
+
+    f1 = h5open("./dataset/ENS_MEM_01/wrfout_d01_2020-04-24_21_00_00", "r")
+    s = HDF5.read(f1)
+
+    temp_values = s["T"]
+    point = [1.1,2.9,3.1]
+    interpolated_temp_value = GI.interpolate(grid,temp_values,point)
+
 end
