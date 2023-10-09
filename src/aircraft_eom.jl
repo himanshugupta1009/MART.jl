c2e6ba3b3d496edfb8223c11f8e8fdc10c13154d
himanshugupta1009 <himanshu.gupta@colorado.edu>
2023-10-08 19:13:50 -0600

    Modify code for Aircraft5 EOM to remove creating states of type AircraftState

--- src/aircraft_eom.jl
+++ src/aircraft_eom.jl
@@ -16,16 +16,17 @@ function AircraftEOM(state,control,wind,noise)
     return SVector(x_dot, y_dot, z_dot, control[2] + noise[4], control[3] + noise[5])
 end
 
-function aircraft_dynamics!(du,u,p,t)
+function aircraft_dynamics(u,p,t)
 
     aircraft_state = u              # [x,y,z,ψ,θ]
     control_inputs = p[1](u,t)      # Control - [Va,ψ_dot,θ_dot]
     wind_inertial = p[2](u,t)       # Wind - [wx,wy,wz]
-    noise = p[3](t)                 # Noise - [nx,ny,nz,nψ,nθ]
+    noise = p[3](t)*0                 # Noise - [nx,ny,nz,nψ,nθ]
     x_dot = AircraftEOM(aircraft_state,control_inputs,wind_inertial,noise)
-    for i in 1:length(u)
-        du[i] = x_dot[i]
-    end
+    return x_dot
+    # for i in 1:length(u)
+    #     du[i] = x_dot[i]
+    # end
     # du[1:length(u)] = x_dot
 end
 
@@ -34,18 +35,19 @@ function aircraft_simulate(dynamics::Function, initial_state, time_interval, ext
 
     prob = DE.ODEProblem(dynamics,initial_state,time_interval,extra_parameters)
     sol = DE.solve(prob,saveat=save_at_value)
-    aircraft_states = AircraftState[]
-    for i in 1:length(sol.u)
-        push!(aircraft_states,AircraftState(sol.u[i]...))
-    end
-    return aircraft_states
+    return sol.u
+    # aircraft_states = AircraftState[]
+    # for i in 1:length(sol.u)
+    #     push!(aircraft_states,AircraftState(sol.u[i]...))
+    # end
+    # return aircraft_states
 end
 
 #=
 true_model_num = 3
-control_func(u,t) = SVector(0.0,0.0,0.0)
+control_func(u,t) = SVector(10.0,0.0,0.0)
 wind_func(u,t) = SVector(0.0,0.0,0.0)
 wind_func(u,t) = fake_wind(u,t,true_model_num)
 noise_func(t) = SVector(0.0,0.0,0.0,0.0,0.0)
-hist = aircraft_simulate(aircraft_dynamics!,[100,100,1800,pi/6,0.0],[0.0,5.0],[control_func,wind_func,noise_func])
+hist = aircraft_simulate(aircraft_dynamics,SVector(100,100,1800,pi/6,0.0),(0.0,5.0),(control_func,wind_func,noise_func))
 =#
