9215007dc60a01f282eeacea641327e8f0b44fca
himanshugupta1009 <himanshu.gupta@colorado.edu>
2024-02-22 13:41:42 -0700

    Add code to run MCTS on belief MDP

---
+++ src/belief_mdp.jl
@@ -0,0 +1,156 @@
+using MCTS
+using POMDPs
+import POMDPModelTools as PMT
+using Random
+include("definitions.jl")
+include("belief_tracker.jl")
+
+
+#BeliefMDP State
+struct MARTBeliefMDPState{M}
+    uav::SVector{5,Float64}
+    belief::SVector{M,Float64}
+    t::Float64
+end
+
+#BeliefMDP Action
+struct MARTBeliefMDPAction <:FieldVector{3,Float64}
+    Va::Float64
+    ψ_dot::Float64
+    θ_dot::Float64
+end
+
+#BeliefMDP
+struct MARTBeliefMDP{T,P,Q} <: POMDPs.MDP{MARTBeliefMDPState,MARTBeliefMDPAction}
+    env::ExperimentEnvironment
+    observation::Function
+    wind::Function
+    noise::Function
+    dvg::T
+    dwg::P
+    png::Q
+    Δt::Float64
+    M::Int64 #Number of Weather Models
+end
+
+function update_belief(m,b0,s0,ctr,o,time_interval)
+
+    num_models = m.M
+    b1 = MVector{num_models,Float64}(undef)
+
+    for i in 1:num_models
+        mwf(X,t) = m.wind(m.dwg,i,X,t)
+        s1_list = aircraft_simulate(aircraft_dynamics,s0,time_interval,
+        (ctr,mwf,no_noise),m.Δt)
+        s1 = s1_list[end]
+        l_pos = transition_likelihood(m.png,o,s1,time_interval[2])
+        l_temp = temperature_likelihood(m.dvg,i,o[6],o,time_interval[2])
+        l_pres = pressure_likelihood(m.dvg,i,o[7],o,time_interval[2])
+        # println(i , " ", s1[2], " ", l_pos, " ", l_temp, " ", l_pres)
+        b1[i] = l_temp*l_pres*l_pos*b0[i]
+    end
+
+    b1 = b1/sum(b1)
+    # return SVector{num_models,Float64}(b1)
+    return SVector(b1)
+end
+
+function calculate_reward(bmdp_s,a,bmdp_sp)
+
+    initial_ent = calculate_entropy(bmdp_s.belief)
+    #=
+    Q: IPFT discounts final_ent by multiplying it with the discount factor γ
+    Should we do that too?
+    =#
+    final_ent = calculate_entropy(bmdp_sp.belief)
+    #=
+    According to the IPFT paper, the information measure is Negative Entropy
+    So, use that as the measure "I".
+    =#
+    r = (-initial_ent) - (-final_ent)
+    return r
+end
+
+function POMDPs.gen(m::MARTBeliefMDP,s,a,rng)
+
+    #Apply given action on the UAV and get the new UAV state
+    curr_uav_state = s.uav
+    curr_belief = s.belief
+    next_t = s.t + m.Δt
+    time_interval = (s.t, next_t)
+    dist = PMT.SparseCat(1:m.M, curr_belief)
+    sampled_model = rand(rng,dist)
+    mwf(X,t) = m.wind(m.dwg,sampled_model,X,t)
+    mof(X,t) = m.observation(m.dvg,sampled_model,X,t)
+    CTR(X,t) = a
+
+    new_state_list = aircraft_simulate(aircraft_dynamics,curr_uav_state,
+                            time_interval,(CTR,mwf,no_noise),m.Δt)
+    new_state = new_state_list[end]
+    process_noise = m.noise(next_t)
+    new_uav_state = add_noise(new_state, process_noise)
+
+    #Sample an observation from the new state
+    o = mof(new_uav_state,next_t)
+
+    #Update the belief for this sampled observation
+    new_belief = update_belief(m,curr_belief,curr_uav_state,CTR,o,time_interval)
+    # new_belief = update_belief(bup,curr_belief,curr_uav_state,o,time_interval)
+
+    #Create the new BeliefMDPState
+    sp = MARTBeliefMDPState(new_uav_state,new_belief,next_t)
+    # println(sp)
+    #Compute the reward R(b,a,b')
+    r = calculate_reward(s,a,sp)
+
+    return (sp=sp,r=r)
+end
+
+
+function POMDPs.actions(mdp::MARTBeliefMDP)
+        return SVector{9,MARTBeliefMDPAction}(
+            MARTBeliefMDPAction(10.0,-2*pi/180,-2*pi/180),
+            MARTBeliefMDPAction(10.0,-2*pi/180,0.0),
+            MARTBeliefMDPAction(10.0,-2*pi/180,2*pi/180),
+            MARTBeliefMDPAction(10.0,0.0,-2*pi/180),
+            MARTBeliefMDPAction(10.0,0.0,0.0),
+            MARTBeliefMDPAction(10.0,0.0,2*pi/180),
+            MARTBeliefMDPAction(10.0,2*pi/180,-2*pi/180),
+            MARTBeliefMDPAction(10.0,2*pi/180,0.0),
+            MARTBeliefMDPAction(10.0,2*pi/180,2*pi/180)
+        )
+end
+
+POMDPs.discount(m::MARTBeliefMDP) = 0.95
+
+#=
+Some analysis
+
+struct T1
+    a::SVector
+    b::Int64
+end
+function f_T1(x)
+    T1(x,11)
+end
+
+struct T2
+    a::SVector{M,Int64} where M
+    b::Int64
+end
+function f_T2(x)
+    T2(x,11)
+end
+
+struct T3
+    a::SVector{2,Int64}
+    b::Int64
+end
+function f_T3(x)
+    T3(x,11)
+end
+
+@benchmark f_T1( SVector(1,2) )
+@benchmark f_T2( SVector(1,2) )
+@benchmark f_T3( SVector(1,2) )
+=#
