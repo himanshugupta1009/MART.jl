c4fff2b9b3ffa96c9dd18e9a514e4a3a2656cc31
himanshugupta1009 <himanshu.gupta@colorado.edu>
2024-02-22 13:39:08 -0700

    Remove unnecessary allocations in belief_tracker.jl code

--- src/belief_tracker.jl
+++ src/belief_tracker.jl
@@ -1,5 +1,5 @@
 using Distributions
-import ComplexityMeasures as CM
+import StatsBase as SB
 
 struct BeliefUpdateParams{T,Q,P}
     dvg::T
@@ -12,7 +12,8 @@ end
 
 
 function temperature_likelihood(dvg,m,o_temp,X,t)
-    temp_mean = dvg.temp_noise_amp[m]*sin(sum(X[1:3])+t)
+    # temp_mean = dvg.temp_noise_amp[m]*sin(sum(X[1:3])+t)
+    temp_mean = dvg.temp_noise_amp[m]*sin( sum(view(X,1:3))+t )
     dist = Normal(temp_mean,sqrt(dvg.temp_noise_amp[m]))
     likelihood = pdf(dist,o_temp)
     return likelihood
@@ -20,7 +21,8 @@ end
 
 
 function pressure_likelihood(dvg,m,o_pressure,X,t)
-    pres_mean = dvg.press_noise_amp[m]*cos(sum(X[1:3])+t)
+    # pres_mean = dvg.press_noise_amp[m]*cos(sum(X[1:3])+t)
+    pres_mean = dvg.press_noise_amp[m]*cos( sum(view(X,1:3))+t )
     dist = Normal(pres_mean, sqrt(dvg.press_noise_amp[m]))
     likelihood = pdf(dist,o_pressure)
     return likelihood
@@ -28,8 +30,12 @@ end
 
 
 function transition_likelihood(png,o_position,X,t)
-    dist = MvNormal(X[1:3],png.covar_matrix)
-    likelihood = pdf(dist,o_position[1:3])
+    # dist = MvNormal(X[1:3],png.covar_matrix)
+    mean = SVector(X[1],X[2],X[3])
+    dist = MvNormal(mean,png.covar_matrix)
+    # likelihood = pdf(dist,o_position[1:3])
+    observed_position = SVector(o_position[1],o_position[2],o_position[3])
+    likelihood = pdf(dist,observed_position)
     return likelihood
 end
 
@@ -57,7 +63,7 @@ function update_belief(bup,b0,s0,o,time_interval)
 end
 
 
-function initialize_belief(::Val{M}) where M
+function get_initial_belief(::Val{M}) where M
     a = fill(1/M, M)
     return SVector{M}(a)
 end
@@ -71,7 +77,7 @@ function final_belief(bup,::Val{M},s,o) where M
     # b0 = SVector{M,Float64}(repeat([1/M],M))
     # b0 = Array{Float64,1}([1/M,1/M,1/M,1/M,1/M,1/M,1/M])
     # b0 = fill(1/M,M)
-    b0 = initialize_belief(Val(M))
+    b0 = get_initial_belief(Val(M))
     B = Array{typeof(b0),1}([b0])
     b_curr = b0
     for i in 1:length(o)
@@ -90,8 +96,11 @@ function calculate_entropy(b)
     The more unimodal a distribution is, the lower its entropy will be.
     Entropy = Lack of Information
     =#
-    p = CM.Probabilities([b...])
-    return CM.entropy(CM.Shannon(),p)
+    # p = CM.Probabilities([b...])
+    # return CM.entropy(CM.Shannon(),p)
+    # println(sum(b))
+    @assert round(sum(b), digits=5) == 1.00000
+    return -SB.entropy(b)
 end
 
 
