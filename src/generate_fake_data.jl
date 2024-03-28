fa344e6e5697f96ded1497704a8b4dfed4cb17b0
himanshugupta1009 <himanshu.gupta@colorado.edu>
2024-03-28 16:28:51 -0600

    Add new functions to generate fake wind that is similar across models

--- src/generate_fake_data.jl
+++ src/generate_fake_data.jl
@@ -33,16 +33,17 @@ end
 
 
 struct DummyWindGenerator{T,P}
-    wind_amplitude::T
-    wind_func::P
+    constant_wind::T
+    wind_models::P
 end
 
 function fake_wind(dwg,M,X,t)
     @assert isinteger(M)
-    wind = dwg.wind_amplitude[M]*dwg.wind_func[M](sum(X[1:3])+t)
-    return SVector{size(dwg.wind_amplitude[M])[1],Float64}(diag(wind))
+    pos = SVector(X[1],X[2],X[3])
+    dist_vec = pos - SVector{3,Float64}(dwg.wind_models[M].μ)
+    wind = dwg.constant_wind + ( dist_vec / norm(dist_vec,2) )
+    return wind
 end
-
 
 struct ProcessNoiseGenerator{T}
     covar_matrix::T
@@ -56,8 +57,54 @@ function process_noise(png,t)
     return SVector{num_state_variables,Float64}(noise)
 end
 
-
 function get_fake_data()
+
+    num_models = 7
+
+    T_noise_amp = SVector{num_models,Float64}(0.6, 0.1, 1.3, 1.1, 0.5, 0.8, 1.7)
+    P_Noise_amp = SVector{num_models,Float64}(1.3, 2.9, 2.3, 0.6, 1.9, 0.1, 1.7)
+    DVG = DummyValuesGenerator(T_noise_amp,P_Noise_amp)
+
+    const_wind = SVector(5.0,7.0,8.0)
+    max_x = max_y = max_z = 3000.0
+    wind_noise_covar = SMatrix{3,3}([
+                        30000.0 0 0;
+                        0 30000.0 0;
+                        0 0 30000.0;
+                        ])
+    wind_models = Vector{MvNormal}(undef,num_models)
+    for i in 1:num_models
+        μ = SVector(floor(rand()*max_x),floor(rand()*max_y),floor(rand()*max_z))
+        σ = wind_noise_covar
+        gaussian_model = MvNormal(μ,σ)
+        wind_models[i] = gaussian_model
+    end    
+    DWG = DummyWindGenerator(const_wind,wind_models)
+
+    noise_covar = SMatrix{3,3}([
+            30000.0 0 0;
+            0 30000.0 0;
+            0 0 30000.0;
+            ])
+    PNG = ProcessNoiseGenerator(noise_covar)
+
+    return DVG,DWG,PNG
+end
+
+#=
+OLD FUNCTIONS TO GENERATE FAKE WIND
+struct OldDummyWindGenerator{T,P}
+    wind_amplitude::T
+    wind_func::P
+end
+
+function old_fake_wind(dwg,M,X,t)
+    @assert isinteger(M)
+    wind = dwg.wind_amplitude[M]*dwg.wind_func[M](sum(X[1:3])+t)
+    return SVector{size(dwg.wind_amplitude[M])[1],Float64}(diag(wind))
+end
+
+function old_get_fake_data()
     T_noise_amp = SVector{7,Float64}(0.6, 0.1, 1.3, 1.1, 0.5, 0.8, 1.7)
     P_Noise_amp = SVector{7,Float64}(1.3, 2.9, 2.3, 0.6, 1.9, 0.1, 1.7)
     DVG = DummyValuesGenerator(T_noise_amp,P_Noise_amp)
@@ -83,6 +130,7 @@ function get_fake_data()
 
     return DVG,DWG,PNG
 end
+=#
 
 #=
 T_noise_amp = SVector{7,Float64}(0.6, 0.1, 1.3, 1.1, 0.5, 0.8, 1.7)
