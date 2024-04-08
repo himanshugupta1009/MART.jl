bdc37548edd7ba41462a8cc69dd1c108ae300e70
Himanshu Gupta <himanshu.gupta@colorado.edu>
2024-04-08 10:46:34 -0600

    Modify the Reward Function

--- src/belief_mdp.jl
+++ src/belief_mdp.jl
@@ -71,7 +71,9 @@ function calculate_reward(bmdp_s,a,bmdp_sp)
     r = (-initial_ent) - (-final_ent)
     =#
     q = bmdp_sp.belief
-    p = SVector(0.0,0.0,0.0,0.0,1.0,0.0,0.0)
+    val,ind = findmax(q)
+    p = MVector(0.0,0.0,0.0,0.0,0.0,0.0,0.0)
+    p[ind] = 1.0
     r = SB.kldivergence(p,q)
     return -r
 end
@@ -109,6 +111,11 @@ function POMDPs.gen(m::MARTBeliefMDP,s,a,rng)
     #Compute the reward R(b,a,b')
     r = calculate_reward(s,a,sp)
     # println(sp, r)
+    if(new_uav_state[1] >= 4500 && new_uav_state[1]<=5500 && new_uav_state[2]>=4500 && new_uav_state[2]<=5500)
+        println("@@@@@@@@@@@ Good News!! Low Noise Region Found. @@@@@@@@@@@")
+        println(sp)
+        println(r)
+    end
     return (sp=sp,r=r)
 end
 
