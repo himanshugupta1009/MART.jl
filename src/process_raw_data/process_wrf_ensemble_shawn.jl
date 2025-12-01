117adff887f91b013cf88e840d061704257aada2
himanshugupta1009 <himanshu.gupta@colorado.edu>
2025-12-01 04:59:08 -0700

    Add code to process Shawn's New Ensemble

--- src/process_raw_data/process_wrf_ensemble_shawn.jl
+++ src/process_raw_data/process_wrf_ensemble_shawn.jl
@@ -1,8 +1,8 @@
 include("src/process_raw_data/read_weather_data.jl")
 
 
-shawn_ensemble_folder = "/media/storage/himanshu_storage/MART/WRF_Ensemble_Shawn/"
-ensemble_member_names = [
+shawn_misaligned_ensemble_folder = "/media/storage/himanshu_storage/MART/WRF_Misaligned_Ensemble_Shawn/"
+misaligned_ensemble_member_names = [
                         "x14_y7_z0",
                         "x14_y13_z0",  
                         "x14_y19_z0",
@@ -16,11 +16,23 @@ ensemble_member_names = [
                         "x26_y19_z0",
                         "x26_y25_z0",
                         ]
-ensemble_member_names_dict = Dict(i => name for (i, name) in 
-                        enumerate(ensemble_member_names))
 
-num_x_cells = 79
-num_y_cells = 63
+shawn_aligned_ensemble_folder = "/media/storage/himanshu_storage/MART/WRF_Ensemble_Shawn/"
+aligned_ensemble_member_names = [
+                        "ctrl/WRF",
+                        "skeb1","skeb2","skeb3","skeb4","skeb5","skeb6","skeb7",
+                        "skeb8","skeb9","skeb10","skeb11","skeb12",
+                        "skeb13","skeb14","skeb15","skeb16","skeb17",
+                        ]
+
+
+shawn_ensemble_folder = shawn_aligned_ensemble_folder
+ensemble_member_names = aligned_ensemble_member_names
+ensemble_member_names_dict = Dict(i => name for (i, name) in 
+                                enumerate(ensemble_member_names))
+
+num_x_cells = 80
+num_y_cells = 64
 num_z_cells = 50
 date_format = dateformat"yyyy-mm-dd_HH:MM:SS"
 start_time = DateTime(2009, 4, 15, 20, 0)  # Example start time
@@ -28,7 +40,7 @@ start_time = DateTime(2009, 4, 15, 20, 0)  # Example start time
 
 em_num = 1
 folder = shawn_ensemble_folder * ensemble_member_names_dict[em_num] * "/run"
-num_timesteps = Int(60*1/5) # 1 hour of data with 5 minute intervals
+num_timesteps = Int(60*2/5) # 2 hours of data with 5 minute intervals
 U,V,W,Z,Z_midpoint,P,T,R = extract_relevant_data(folder = folder,
                                                 num_x_cells=num_x_cells,
                                                 num_y_cells=num_y_cells,
@@ -41,7 +53,7 @@ U,V,W,Z,Z_midpoint,P,T,R = extract_relevant_data(folder = folder,
 
 
 em_num = 1
-num_timesteps = Int(60*1/5) # 1 hour of data with 5 minute intervals
+num_timesteps = Int(60*2/5) # 2 hours of data with 5 minute intervals
 source_folder = shawn_ensemble_folder * ensemble_member_names_dict[em_num] * "/run"
 output_folder = "/media/storage/himanshu_storage/MART/Processed_WRF_Ensemble_Shawn"
 generate_relevant_dataset_wrf_nc(model_num = em_num, 
@@ -57,7 +69,7 @@ generate_relevant_dataset_wrf_nc(model_num = em_num,
 
 
 
-num_timesteps = Int(60*1/5) # 1 hour of data with 5 minute intervals
+num_timesteps = Int(60*2/5) # 2 hours of data with 5 minute intervals
 output_folder = "/media/storage/himanshu_storage/MART/Processed_WRF_Ensemble_Shawn"
 num_ensemble_members = length(ensemble_member_names)
 for em_num in 1:num_ensemble_members
@@ -76,8 +88,8 @@ end
 
 data_folder = "/media/storage/himanshu_storage/MART/Processed_WRF_Ensemble_Shawn/"
 wm = WeatherModels([1,2,3,4,5,6,7],12,data_folder,
-                    num_x_points=79,
-                    num_y_points=63,
+                    num_x_points=80,
+                    num_y_points=64,
                     num_z_points=50);
 get_scalar_value(wm, 3, 30030,45678,15000, 20, :P)
 get_vector_value(wm, 3, 30030,45678,15000, 20, :U)
